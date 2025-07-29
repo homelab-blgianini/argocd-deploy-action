169c8a93fcc03cf1bee5be08401b2753d52c925f
Bruno Gianini <blgianini@gmail.com>
2025-07-28 21:38:48 -0300

    fix: testes mais eficientes

--- src/index.js
+++ src/index.js
@@ -23,19 +23,12 @@ async function getToken(senha){
 }
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("nome-aplicacao");
-  core.info(`Hello ${nameToGreet}!`);
-
   core.info(await getToken(core.getInput("token-argocd")))
 
   // Get the current time and set it as an output variable
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  core.info(`The event payload: ${payload}`);
+  
 } catch (error) {
   core.setFailed(error.message);
 }
